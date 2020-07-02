de390725e990d60ef104f5d3d98f7a80d2c38d88
baby2nana <jinbao5488@163.com>
2020-07-02 16:47:45 +0800

    Network go on

---
+++ Assets/MrPP.com/MrPP/Content/Manager/MrPPNetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MrPP.Network{
+
+
+public class MrPPNetworkManager : NetworkManager
+{
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnServerConnect(conn);
+        Debug.Log("A client connect to server:" + conn);
+
+    }
+     
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnServerDisconnect(conn);
+        Debug.Log("A client disconnect to server:" + conn);
+    }
+
+    public override void OnServerError(NetworkConnection conn, int errorCode)
+    {
+        base.OnServerError(conn,errorCode);
+        Debug.Log("Server network error occurred: " + (NetworkError)errorCode);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log("Connected successfully to server,now to set up buff for the client" + conn);
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        Debug.Log("client disconnect from server" + conn);
+    }
+
+    public override void OnClientError(NetworkConnection conn, int errorCode)
+    {
+        base.OnClientError(conn,errorCode);
+        Debug.Log("client network error occurred: " + (NetworkError)errorCode);
+    }
+
+}
+}
---
+++ Assets/MrPP.com/MrPP/Content/Manager/NetworkDiscoveryReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MrPP.Network
+{
+    
+public class NetworkDiscoveryReceiver : NetworkDiscovery
+{
+    public delegate void Receiver(string fromAddress,string data);
+
+    public event Receiver onReceiver;
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        if(onReceiver != null)
+        {
+            onReceiver(fromAddress,data);
+        }
+    }
+
+}
+
+
+}
