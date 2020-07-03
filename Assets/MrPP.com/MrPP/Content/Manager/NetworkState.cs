50199b0f173d9bbb4d70d0d15bb3e2f88316d9f2
baby2nana <jinbao5488@163.com>
2020-07-03 17:07:18 +0800

    network can test,move on

--- Assets/MrPP.com/MrPP/Content/Manager/NetworkState.cs
+++ Assets/MrPP.com/MrPP/Content/Manager/NetworkState.cs
@@ -39,23 +39,132 @@ public class NetworkState : MonoBehaviour
     private FSM fsm_ = new FSM();
     // Start is called before the first frame update
 
-    private StateBase beginState()
+    public void doBroadcast()
     {
-        StateBase state = new StateBase();
-        //state.
+        fsm_.post("broadcast");
+    }
+
+    public void doListening()
+    {
+        fsm_.post("listening");
+    }
+    private State beginState()
+    {
+        State state = new State();
+        state.addAction("broadcast","broadcast");
+        state.addAction("listening","listening");
+        return state;
+    }
+
+    private State hostState()
+    {
+        State state = new State();
+        state.onStart += delegate
+        {
+            _system.startHost();
+        };
+
+        //state.addAction("client","client");
+        state.onOver += delegate
+        {
+            _system.stopHost();
+        };
+        return state;
+    }
+
+    public void doRunning()
+    {
+        fsm_.post("running");
+    }
+    private State broadcastState()
+    {
+        State state = new State();
+        state.onStart += delegate
+        {
+            _system.startBroadcast();
+        };
+
+        state.addAction("running","running");
+        state.onOver += delegate
+        {
+            _system.stopBroadcast();
+        };
+        return state;
+    }
+
+    private State runningState()
+    {
+        State state = new State();
+        state.addAction("broadcast","broadcast");
+        return state;
+    }
+    public void doJoin()
+    {
+        fsm_.post("join");
+    }
+
+    public void doAlone()
+    {
+        fsm_.post("alone");
+    }
+    private State listeningState()
+    {
+        State state = new State();
+        state.onStart += delegate
+        {
+            _system.startListening();
+        };
+
+        state.addAction("join","client");
+        state.addAction("alone","alone");
+
+        state.onOver += delegate
+        {
+            _system.stopListening();
+        };
+        return state;
+    }
+
+    private State clientState()
+    {
+        State state = new State();
+        state.onStart += delegate
+        {
+            _system.startClient();
+        };
+
+        state.onOver += delegate
+        {
+            _system.stopClient();
+        };
+        return state;
+    }
+
+    private State aloneState()
+    {
+        State state = new State();
+        state.onStart += delegate
+        {
+            _system.startHost();
+        };
+
+        state.onOver += delegate
+        {
+            _system.stopHost();
+        };
         return state;
     }
     void Start()
     {
         fsm_.addState("begin",beginState());
-        // fsm_.addState("host",hostState());
-        // fsm_.addState("broadcast",broadcastState(),"host");
-        // fsm_.addState("running",runningState(),"host");
+        fsm_.addState("host",hostState());
+        fsm_.addState("broadcast",broadcastState(),"host");
+        fsm_.addState("running",runningState(),"host");
 
-        // fsm_.addState("alone",aloneState());
+        fsm_.addState("alone",aloneState());
 
-        // fsm_.addState("listening",listeningState());
-        // fsm_.addState("client",clientState());
+        fsm_.addState("listening",listeningState());
+        fsm_.addState("client",clientState());
 
         fsm_.init("begin");
 
