8c36a3c69b2d18def363ec013f405a823905d0e5
翟金保 <jinbao5488@163.com>
2020-07-08 11:36:42 +0800

    plane finder sender and receiver Add

---
+++ Assets/MrPP.com/MrPP/Content/Arrange/Content/SurperView/Mobile/PlaneFinderReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using GDGeek;
+
+namespace MrPP.SuperView
+{
+
+
+    public class PlaneFinderReceiver : Singleton<PlaneFinderReceiver>
+    {
+        public Action OnFound { get; set; }
+        public Action OnLost { get; set; }
+
+        public void doFound()
+        {
+            OnFound?.Invoke();
+        }
+
+        public void doLost()
+        {
+            OnLost?.Invoke();
+        }
+    }
+}
---
+++ Assets/MrPP.com/MrPP/Content/Arrange/Content/SurperView/Mobile/PlaneFinderSender.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GDGeek;
+namespace MrPP.SuperView
+{
+
+    public class PlaneFinderSender : Singleton<PlaneFinderSender>
+    {
+        private bool antomaticHitTest_ = false;
+        private bool antomaticHit_ = false;
+
+
+        public void onInteractiveHitTest()
+        {
+
+        }
+        public void onAutomaticHitTest()
+        {
+            antomaticHitTest_ = true;
+        }
+
+        private void doFound()
+        {
+            PlaneFinderReceiver.Instance?.doFound();
+        }
+
+        private void doLost()
+        {
+            PlaneFinderReceiver.Instance?.doLost();
+        }
+
+        private void LateUpdate()
+        {
+            if(antomaticHit_ != antomaticHitTest_)
+            {
+                antomaticHit_ = antomaticHitTest_;
+
+                if(antomaticHit_)
+                {
+                    Debug.Log("onFound ...");
+                    doFound();
+                }else{
+                    doLost();
+                }
+                antomaticHitTest_ = false;
+            }
+        }
+    }
+}
